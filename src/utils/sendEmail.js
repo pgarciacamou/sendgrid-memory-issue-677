1a96036085606560033a799f69551c25a12f5861
Pablo Garcia <pgarciacamou@gmail.com>
2018-04-24 13:32:51 -0700

    using @sendgrid/mail module

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,12 +1,13 @@
 const _ = require("lodash");
 const Promise = require("promise-polyfill");
 const logger = require("heroku-logger");
-const sendgrid = require("sendgrid");
+const sgMail = require("@sendgrid/mail");
 const ejs = require("ejs");
 const envConfig = require("../../env.config.js");
-const sg = sendgrid(envConfig.SENDGRID_API_KEY);
 
-const toPromiseEmailRequest = ({ to, from, subject, template, data }) => {
+sgMail.setApiKey(envConfig.SENDGRID_API_KEY);
+
+const parseTemplateAndSend = ({ to, from, subject, template, data }) => {
   return new Promise((resolve, reject) => {
     ejs.renderFile(template, data, (err, html) => {
       if (err) {
@@ -19,40 +20,29 @@ const toPromiseEmailRequest = ({ to, from, subject, template, data }) => {
         });
       }
 
-      const request = sg.emptyRequest({
-        method: "POST",
-        path: "/v3/mail/send",
-        body: {
-          personalizations: [{
-            to: _.map(to, (email) => ({ email })),
-            subject: subject
-          }],
-          from: { email: from },
-          content: [{
-            type: "text/html",
-            value: html
-          }]
-        }
-      });
-
-      sg.API(request).then((response) => {
-        logger.info("sendgrid", response);
-        resolve(response);
-      }).catch((error) => {
-        // error is an instance of SendGridError
-        // The full response is attached to error.response
-        logger.info("sendgrid::error", error);
-        reject({
-          error,
-          message: "There was an error sending the email."
+      // this implementation has to be changed to send multiple emails at the
+      // same time instead, but for the purposes of this demo, this is the only
+      // thing that was changed and tested.
+      sgMail
+        .send({ to, from, subject, html })
+        .then((response) => {
+          logger.info("sendgrid", response);
+          resolve(response);
+        }).catch((error) => {
+          // error is an instance of SendGridError
+          // The full response is attached to error.response
+          logger.info("sendgrid::error", error);
+          reject({
+            error,
+            message: "There was an error sending the email."
+          });
         });
-      });
     });
   });
 };
 
 function sendEmail(emails) {
-  return Promise.all(_.map(emails, toPromiseEmailRequest));
+  return Promise.all(_.map(emails, parseTemplateAndSend));
 }
 
 module.exports = sendEmail;
